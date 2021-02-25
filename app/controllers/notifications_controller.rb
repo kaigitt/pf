3bc4a95a4dfed629403ad6fbbeb5930d3ab318da
kaigitt <s12201600350@gmail.com>
2021-02-25 09:41:23 +0000

    check1

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -5,7 +5,7 @@ class NotificationsController < ApplicationController
       @notifications = current_user.passive_notifications
     #@notificationの中でまだ確認していない(indexに一度も遷移していない)通知のみ
       @notifications.where(checked: false).each do |notification|
-          notification.update_attributes(checked: true)
+        otification.update_attributes(checked: true)
       end
   end
 
