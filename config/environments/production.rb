333308e4e9b55be312449390d6cbc9f7ad186f9f
kaigitt <s12201600350@gmail.com>
2021-02-27 03:52:41 +0000

    [fix] contacts production.config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,15 +70,12 @@ Rails.application.configure do
   #送信方法として:smtpを指定した場合は、このconfigを使って送信詳細の設定を行います
   config.action_mailer.smtp_settings = {
     #gmail利用時はaddress,domain,portは下記で固定
-    address:"smtp.gmail.com",
-    domain: 'gmail.com',
-    port:587,
-    #gmailのユーザアカウント（xxxx@gmail.com)※念のため、credentials.yml.enc行き
-    user_name: Rails.application.credentials.gmail[:user_name],
-    #gmail２段階認証回避のためにアプリケーションでの利用パスワードを取得、必ずcredentials.yml.endに設定を！！
-    password: Rails.application.credentials.gmail[:password],
-    #パスワードをBase64でエンコード
-    authentication: :login
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'gmail.com',
+    user_name:            ENV['APP_EMAIL'],
+    password:             ENV['APP_PASSWORD'],
+    enable_starttls_auto: true
   }
 
   config.action_mailer.perform_caching = false
