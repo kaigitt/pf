2763ffb0087b7e091651f0bc4eb957aff6b3f94b
kaigitt <s12201600350@gmail.com>
2021-03-11 15:37:00 +0000

    [fix4]

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,34 +1,34 @@
-class RoomChannel < ApplicationCable::Channel
-  def subscribed
-    stream_from "room_channel_#{params['room']}"
-    stream_for current_user.id
-  end
+# class RoomChannel < ApplicationCable::Channel
+#   def subscribed
+#     stream_from "room_channel_#{params['room']}"
+#     stream_for current_user.id
+#   end
 
-  def unsubscribed
-  end
+#   def unsubscribed
+#   end
 
-  def speak(data)
-  end
+#   def speak(data)
+#   end
 
-  # def speak(data)
-  #   Message.create!(body: data['message'], room_id: data['room'], user_id: current_user.id)
-  #       if data["message"]
-  #     Message.create!(
-  #       user_id: current_user.id,
-  #       room_id: data['room'],
-  #       message: data["message"]
-  #     )
-  #     #画面を開いているのがチャット送信者だった場合
-  #     ChatChannel.broadcast_to current_user.id,
-  #       message: data["message"],
-  #       room_id: data['room'],
-  #       isCurrent_user: true
+#   # def speak(data)
+#   #   Message.create!(body: data['message'], room_id: data['room'], user_id: current_user.id)
+#   #       if data["message"]
+#   #     Message.create!(
+#   #       user_id: current_user.id,
+#   #       room_id: data['room'],
+#   #       message: data["message"]
+#   #     )
+#   #     #画面を開いているのがチャット送信者だった場合
+#   #     ChatChannel.broadcast_to current_user.id,
+#   #       message: data["message"],
+#   #       room_id: data['room'],
+#   #       isCurrent_user: true
 
-  #     #画面を開いているのがチャット受信者だった場合
-  #     ChatChannel.broadcast_to data["partner_id"].to_i,
-  #       message: data["message"],
-  #       room_id: data['room'],
-  #       isCurrent_user: false
-  #   end
-  # end
-end
+#   #     #画面を開いているのがチャット受信者だった場合
+#   #     ChatChannel.broadcast_to data["partner_id"].to_i,
+#   #       message: data["message"],
+#   #       room_id: data['room'],
+#   #       isCurrent_user: false
+#   #   end
+#   # end
+# end
